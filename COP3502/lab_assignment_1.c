f035cd80f70752deae358a02498548d091dd74a9
MikeUCF <130028967+MikeUCF@users.noreply.github.com>
2023-05-20 23:55:13 -0400

    import of pointer array 2 file

--- COP3502/lab_assignment_1.c
+++ COP3502/lab_assignment_1.c
@@ -1,1 +1,41 @@
 
+/****************
+                 *
+                 *
+                 *
+****************/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int arr1[5][5]= {{0,1,2,3,4},
+                     {1,2,3,4,5},
+                     {2,3,4,5,6},
+                     {3,4,5,6,7},
+                     {4,5,6,7,8}};
+
+// Creating 2D array of pointers using Dynamic Memory
+// Allocation through malloc()
+int*** arr2 = malloc(5 * sizeof(int**));
+    int i,j;
+for(i = 0; i<5; i++){
+    arr2[i] = malloc(5 * sizeof(int*));
+}
+//Initialising each element of the
+//pointer array with the address of
+//element present in the other array
+for(int i = 0; i < 5; i++){
+    for(j = 0; j < 5; j++){
+        arr2[i][j] = &arr1[i][j];
+    }
+}
+//printing the array using 
+//the array pointers
+for(int i = 0; i < 5; i++){
+    for(j = 0; j < 5; j++){
+        printf("%d",&arr2[i][j]);
+    }
+    printf("\n");
+}
+return 0;
+}
