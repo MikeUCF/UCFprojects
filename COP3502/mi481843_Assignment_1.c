0e88ec993b4c93067ac739798f5f3a567691f767
MikeUCF <130028967+MikeUCF@users.noreply.github.com>
2023-06-01 06:02:55 -0400

    Implementation of delete_present_list

--- COP3502/mi481843_Assignment_1.c
+++ COP3502/mi481843_Assignment_1.c
@@ -28,11 +28,22 @@ for(int i = 0; i < 4; i++){
     christmasList[i].present_name = malloc(char*)malloc(20*sizeof(char))
     christmasList[i].present_name = "Present";
 }
+if(delete_present_list(&christmasList, 4) != NULL){
+    printf("Delete Unsuccessful.");
+}
+else{
+    printf("Delete Successful.");
+}
 
 }//end of main()
 
 struct present* delete_present_list(struct present* present_list, int num){
-
+for(int i = 0; i < num; i++){
+    if(present_list[i] != NULL && present_list[i].present_name != NULL){
+        free(present_list[i].present_name);
+    }
+}
+free(present_list);
 }//end of delete_present_list()
 
 void myFlush(){
