df514e401f0f924d87a5c9edc55154cee70bc1ce
MikeUCF <130028967+MikeUCF@users.noreply.github.com>
2023-06-03 18:37:35 -0400

    Update mi481843_Assignment_1.c

--- COP3502/mi481843_Assignment_1.c
+++ COP3502/mi481843_Assignment_1.c
@@ -1,5 +1,5 @@
 /*******************************
-**written by : Mike Castor     *   
+**written by : Mike Castor     *
 **Date written: June 1         *
 **Purpose: Freeing dynamic Mem *
 ********************************/
@@ -11,6 +11,7 @@
 #define CLS System("cls")
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //prototype functions
 struct present* delete_present_list(struct present* present_list, int num);
@@ -18,35 +19,45 @@ void myFlush();
 
 //structs
 struct present{
-char* present_name;
-float price;
+    char* present_name;
+    float price;
 };
 
 int main(){
-struct present* christmasList = (struct present*)malloc(4*sizeof(struct present));
-for(int i = 0; i < 4; i++){
-    christmasList[i].present_name = malloc(char*)malloc(20*sizeof(char))
-    christmasList[i].present_name = "Present";
-}
-if(delete_present_list(&christmasList, 4) != NULL){
-    printf("Delete Unsuccessful.");
-}
-else{
-    printf("Delete Successful.");
-}
+    struct present* christmasList = (struct present*)malloc(4*sizeof(struct present));
 
+    if (christmasList == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    for(int i = 0; i < 4; i++){
+        christmasList[i].present_name = (char*)malloc(20*sizeof(char));
+        strcpy(christmasList[i].present_name, "present");
+    }
+
+    if(delete_present_list(christmasList, 4) != NULL){
+        printf("Delete Unsuccessful.");
+    }
+    else{
+        printf("Delete Successful.");
+    }
+    return 0;
 }//end of main()
 
 struct present* delete_present_list(struct present* present_list, int num){
-for(int i = 0; i < num; i++){
-    if(present_list[i] != NULL && present_list[i].present_name != NULL){
-        free(present_list[i].present_name);
+    if(present_list == NULL)
+        return NULL;
+    for(int i = 0; i < num; i++){
+        if(present_list[i].present_name != NULL)
+            free(present_list[i].present_name);
+
     }
-}
-free(present_list);
+    free(present_list);
+    present_list = NULL;
+    return present_list;
 }//end of delete_present_list()
 
 void myFlush(){
     while(getchar() != '\n');
 }//end of myFlush()
-
